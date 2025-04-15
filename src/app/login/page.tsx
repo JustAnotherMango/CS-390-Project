9853c213588732aef509dd5fb166b2d5305ef5d6
Josh <jsvogel03@gmail.com>
2025-04-15 12:38:51 -0500

    Drafted a design
    Design is pretty much good to go, just need to finalize the position of a few elements and add a back button. Backend still needs done.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from '../../components/ui/LoginForm';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-900">
+    <div className="w-full bg-cover bg-center items-center justify-center flex flex-col min-h-screen" style={{ backgroundImage: "url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F1393758.jpg&f=1&nofb=1&ipt=74b937665e72726adc97d411b6799bc63c9358be296c369dafa1c510891deb62')" }}>
       <main className="flex-grow container w-1/2 mx-auto p-6 content-center">
         <div className="bg-gray-800 items-center justify-items-center rounded-lg p-4" >
           <h1 className="text-white text-3xl text-center pb-4">Politrades</h1>
@@ -16,7 +16,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="bg-gray-950 text-white py-6 text-center">
+      <footer className="bg-gray-950 text-white py-6 text-center w-full">
         <p>Footer info here - you made it to the bottom!</p>
       </footer>
     </div>
--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -27,28 +27,31 @@ export default function LoginPage() {
   };
 
   return (
-      <form className="flex flex-col items-center" onSubmit={handleLogin}>
-        <input 
-          className="text-white border-b-2 border-white-2 w-8/12"
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          className="text-white border-b-2 border-white-2 w-8/12 pt-3"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <span className="py-2"></span>
-        <button className="text-white border-2 border-white-2 rounded-lg py-2 px-5 cursor-pointer bg-green-600 border-green-600 hover:text-green-600 hover:bg-gray-700" type="submit">Login</button>
-        <p>{message}</p>
-        <p className="text-white cursor-pointer hover:text-green-600 pt-2">Forgot Password</p>
-        <p className="text-white cursor-pointer hover:text-green-600">Create Account</p>
-      </form>
+        <form className="flex flex-col items-center" onSubmit={handleLogin}>
+          <input 
+            className="text-white border-b-2 border-white-2 w-8/12"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <input
+            className="text-white border-b-2 border-white-2 w-8/12 pt-3"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <span className="py-2"></span>
+          <p className="text-white cursor-pointer hover:text-green-600 pb-3 underline">Forgot Password</p>
+          <button className="text-white border-2 border-white-2 rounded-lg py-2 px-10 cursor-pointer bg-green-600 border-green-600 hover:text-green-600 hover:bg-gray-700" type="submit">Login</button>
+          <p>{message}</p>
+          <div className="flex pt-2">
+            <p className="text-white pr-1">Don't have an account?</p>
+            <p className="text-white cursor-pointer hover:text-green-600 pb-3 underline">Register</p>
+          </div>
+        </form>
   );
 }
